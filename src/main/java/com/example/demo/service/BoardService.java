aad7d90002b8278238a0a62efa94a3098a7d51e3
mut-jin <dkssud2422@naver.com>
2023-04-26 15:29:23 +0900

    20230426

---
+++ src/main/java/com/example/demo/service/BoardService.java
@@ -0,0 +1,39 @@
+package com.example.demo.service;
+
+import java.util.*;
+
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.stereotype.*;
+
+import com.example.demo.domain.*;
+import com.example.demo.mapper.*;
+
+@Service
+public class BoardService {
+
+	@Autowired
+	private BoardMapper mapper;
+	
+	public List<Board> listBoard() {
+		List<Board> list = mapper.selectAll();
+		return list;
+	}
+
+	public Board getBoard(Integer id) {
+		// TODO Auto-generated method stub
+		return mapper.selectById(id);
+	}
+
+	public boolean modify(Board board) {
+		// TODO Auto-generated method stub
+		int cnt = mapper.update(board);
+		
+		return cnt == 1;
+	}
+
+	public boolean remove(Integer id) {
+		int cnt = mapper.deleteById(id);
+		return cnt == 1;
+	}
+	
+}
