dd489cf6c98a51cd62d01fdc3a86dfe6a7dcc4fa
mut-jin <dkssud2422@naver.com>
2023-05-03 17:02:54 +0900

    20230503

--- src/main/java/com/example/demo/mapper/BoardMapper.java
+++ src/main/java/com/example/demo/mapper/BoardMapper.java
@@ -115,6 +115,25 @@ public interface BoardMapper {
 			VALUES (#{boardId}, #{fileName})
 			""")
 	Integer insertFileName(Integer boardId, String fileName);
+
+	@Select("""
+			SELECT fileName FROM FileName
+			WHERE boardId = #{boardId}
+			""")
+	List<String> selectFileNameByBoardId(Integer boardid);
+
+	@Delete("""
+			DELETE FROM FileName
+			WHERE boardId = #{boardId}
+			""")
+	void deleteFileNameByBoardId(Integer boardId);
+
+	@Delete("""
+			DELETE FROM FileName
+			WHERE boardId = #{boardId}
+				AND fileName = #{fileName}
+			""")
+	void deleteFileNameByBoardIdAndFileName(Integer boardId, String fileName);
 	
 
 	
